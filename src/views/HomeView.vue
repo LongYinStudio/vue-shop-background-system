f45a7ef31fed3796fce6e42e5d4a4d06645b92dd
LongYinStudio <1606776851@qq.com>
2023-06-07 23:05:41 +0800

    添加Element-UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949">
+    </el-switch>
   </div>
 </template>
 
@@ -10,6 +12,9 @@
 
 export default {
   name: "HomeView",
+  data() {
+    return {};
+  },
   components: {},
 };
 </script>
