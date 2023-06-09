d95bbcf5dd0dd65ae11252998319d6bb24954a8a
LongYinStudio <1606776851@qq.com>
2023-06-09 15:36:45 +0800

    修改HomeView的图表

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,24 +49,19 @@ export default {
   mounted() {
     this.$echarts.init(this.$refs.home_chart).setOption({
       title: {
-        text: "Stacked Line",
+        text: "数据统计",
       },
       tooltip: {
         trigger: "axis",
       },
       legend: {
-        data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"],
+        data: ["订单", "销售额", "注册人数", "管理员人数"],
       },
       grid: {
         left: "3%",
         right: "4%",
         bottom: "3%",
         containLabel: true,
-      },
-      toolbox: {
-        feature: {
-          saveAsImage: {},
-        },
       },
       xAxis: {
         type: "category",
@@ -78,34 +73,28 @@ export default {
       },
       series: [
         {
-          name: "Email",
+          name: "订单",
           type: "line",
           stack: "Total",
           data: [120, 132, 101, 134, 90, 230, 210],
         },
         {
-          name: "Union Ads",
+          name: "销售额",
           type: "line",
           stack: "Total",
           data: [220, 182, 191, 234, 290, 330, 310],
         },
         {
-          name: "Video Ads",
+          name: "注册人数",
           type: "line",
           stack: "Total",
           data: [150, 232, 201, 154, 190, 330, 410],
         },
         {
-          name: "Direct",
+          name: "管理员人数",
           type: "line",
           stack: "Total",
           data: [320, 332, 301, 334, 390, 330, 320],
-        },
-        {
-          name: "Search Engine",
-          type: "line",
-          stack: "Total",
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
         },
       ],
     });
