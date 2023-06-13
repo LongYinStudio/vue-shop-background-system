60fa9554e9efe7ede95dd5b5acb7ae690ebda71e
LongYinStudio <1606776851@qq.com>
2023-06-13 12:29:58 +0800

    完成orders 编辑

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -44,7 +44,7 @@
         </el-row>
       </el-form>
       <el-table :data="tableData" border style="width: 100%">
-        <el-table-column fixed prop="orderId" label="订单号" width="150">
+        <el-table-column fixed prop="orderId" label="订单号" width="120">
         </el-table-column>
         <el-table-column prop="time" label="下单时间" width="120">
           <template slot-scope="scope">
@@ -54,10 +54,40 @@
           </template>
         </el-table-column>
         <el-table-column prop="phone" label="手机号" width="120">
+          <template slot-scope="scope">
+            <el-input
+              v-if="scope.$index === editIndex"
+              v-model="scope.row.phone"
+              placeholder="手机号"
+            ></el-input>
+            <div v-else>
+              {{ scope.row.phone }}
+            </div>
+          </template>
         </el-table-column>
         <el-table-column prop="consignee" label="收货人" width="120">
+          <template slot-scope="scope">
+            <el-input
+              v-if="scope.$index === editIndex"
+              v-model="scope.row.consignee"
+              placeholder="收货人"
+            ></el-input>
+            <div v-else>
+              {{ scope.row.consignee }}
+            </div>
+          </template>
         </el-table-column>
         <el-table-column prop="address" label="地址" width="180">
+          <template slot-scope="scope">
+            <el-input
+              v-if="scope.$index === editIndex"
+              v-model="scope.row.address"
+              placeholder="地址"
+            ></el-input>
+            <div v-else>
+              {{ scope.row.address }}
+            </div>
+          </template>
         </el-table-column>
         <el-table-column prop="sendTime" label="送达时间" width="120">
           <template slot-scope="scope">
@@ -67,17 +97,64 @@
           </template>
         </el-table-column>
         <el-table-column prop="tips" label="用户备注" width="120">
+          <template slot-scope="scope">
+            <el-input
+              v-if="scope.$index === editIndex"
+              v-model="scope.row.tips"
+              placeholder="用户备注"
+            ></el-input>
+            <div v-else>
+              {{ scope.row.tips }}
+            </div>
+          </template>
         </el-table-column>
         <el-table-column prop="money" label="订单金额" width="120">
+          <template slot-scope="scope">
+            <el-input
+              v-if="scope.$index === editIndex"
+              v-model="scope.row.money"
+              placeholder="订单金额"
+            ></el-input>
+            <div v-else>
+              {{ scope.row.tips }}
+            </div>
+          </template>
         </el-table-column>
         <el-table-column prop="state" label="订单状态" width="120">
+          <template slot-scope="scope">
+            <el-select
+              v-if="scope.$index === editIndex"
+              v-model="scope.row.state"
+              placeholder="订单状态"
+            >
+              <el-option label="已完成" value="已完成"></el-option>
+              <el-option label="派送中" value="派送中"></el-option>
+              <el-option label="已受理" value="已受理"></el-option>
+            </el-select>
+            <div v-else>
+              {{ scope.row.state }}
+            </div>
+          </template>
         </el-table-column>
         <el-table-column fixed="right" label="操作" width="100">
           <template slot-scope="scope">
             <el-button @click="handleClick(scope.row)" type="text" size="small"
               >查看</el-button
             >
-            <el-button type="text" size="small">编辑</el-button>
+            <el-button
+              type="text"
+              size="small"
+              v-if="scope.$index === editIndex"
+              @click="save(scope.$index, scope.row)"
+              >保存</el-button
+            >
+            <el-button
+              type="text"
+              size="small"
+              v-else
+              @click="handleEdit(scope.$index, scope.row)"
+              >编辑</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
@@ -103,17 +180,20 @@ export default {
         phoneNum: "",
         state: "",
       },
+      editForm: {
+        phone: -1,
+      },
       value1: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
       orders: [],
       pageSize: 6,
       currentPage: 1,
       tableData: [],
       total: -1,
+      editIndex: -1,
     };
   },
   methods: {
     onSubmit() {
-      // alert("查询");
       this.getClasses();
     },
     handleClick(row) {
@@ -133,7 +213,7 @@ export default {
         queryParams += "&phone=" + this.ordersForm.phoneNum;
       if (this.ordersForm.state)
         queryParams += "&orderState=" + this.ordersForm.state;
-      console.log(queryParams);
+      // console.log(queryParams);
       this.$axios
         .get(
           "http://localhost:5000/order/list?currentPage=" +
@@ -147,6 +227,7 @@ export default {
           this.tableData = [];
           res.data.data.forEach((element) => {
             this.tableData.push({
+              id: element.id,
               orderId: element.orderNo,
               time: element.orderTime,
               phone: element.phone,
@@ -161,6 +242,54 @@ export default {
         })
         .catch((err) => {
           console.log(err);
+        });
+    },
+    handleEdit(index, row) {
+      this.editIndex = index;
+      row.edit = true;
+    },
+    save(index, row) {
+      this.editIndex = -1;
+      this.$axios
+        .post("http://localhost:5000/order/edit", {
+          id: row.id,
+          orderNo: row.orderId,
+          orderTime: row.time,
+          phone: row.phone,
+          consignee: row.consignee,
+          deliverAddress: row.address,
+          deliveryTime: row.sendTime,
+          remarks: row.tips,
+          orderAmount: row.money,
+          orderState: row.state,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.data.code === 0) {
+            this.$message({
+              message: "修改成功",
+              type: "success",
+            });
+          }
+          if (res.data.code === 1) {
+            this.$message({
+              message: "修改失败",
+              type: "error",
+            });
+          }
+          if (res.data.code === 5001) {
+            this.$message({
+              message: "参数错误",
+              type: "error",
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$message({
+            message: "连接后台失败",
+            type: "error",
+          });
         });
     },
   },
